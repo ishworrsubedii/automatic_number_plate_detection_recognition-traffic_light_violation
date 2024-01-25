bffb264ebab10fa242dd8f2f164448e46acd828e
Ishwor Subedi <ishworhero21@gmail.com>
2024-01-25 21:44:44 +0545

    Enhance Dashboard with theme and header

--- frontend-trinetra/src/scenes/dashboard/Dashboard.jsx
+++ frontend-trinetra/src/scenes/dashboard/Dashboard.jsx
@@ -1,8 +1,7 @@
 import { Box, IconButton, useTheme, Divider } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../theme";
-import InputBase from "@mui/material/InputBase";
-import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import Header from '../../components/Header.jsx';import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
@@ -12,7 +11,14 @@ const Dashboard = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     return (
-        <div></div>
+        <Box margin="20px">
+            <Box display="flex" justifyContent={"space-between"}alignContent={"center"}>
+
+            <Header title="DASHBOARD" subtitle="Welcome to your dashboard">
+
+                </Header>
+            </Box>
+        </Box>
     );
 }
 export default Dashboard;
