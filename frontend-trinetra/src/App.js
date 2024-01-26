b9ab68f75da12cf21329886559efe226ca2c0d4e
Ishwor Subedi <ishworhero21@gmail.com>
2024-01-26 16:04:54 +0545

    Refactor: Update import statements and initial state in App.js

--- frontend-trinetra/src/App.js
+++ frontend-trinetra/src/App.js
@@ -1,9 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 
-
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard/Dashboard";
@@ -21,35 +20,58 @@ import Blog from "./scenes/blog/Blog";
 import CameraList from "./scenes/cameralist/CameraStatus";
 import Notification from "./scenes/notification/Notification";
 
+import LoginPage from "./scenes/auth/login/Login";
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+
+    setLoggedIn(true);
+    navigate("/");
+  };
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate("/login");
+    }
+  }, [isLoggedIn, navigate]);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-
         <div className="app">
-          <Sidebar isSidebar={isSidebar} />
+          {isLoggedIn && <Sidebar isSidebar={isSidebar} />}
           <main className="content">
-            <Topbar setIsSidebar={setIsSidebar} />
+            {isLoggedIn && <Topbar setIsSidebar={setIsSidebar} />}
             <Routes>
-              <Route path="/" element={<Dashboard />} />
-
-              {/* <Route path="/server-status" element={<ServerStatus />} />
+              <Route
+                path="/"
+                element={isLoggedIn ? <Dashboard /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/login"
+                element={<LoginPage onLogin={handleLogin} />}
+              />
+              <Route path="/server-status" element={<ServerStatus />} />
               <Route path="/overview" element={<Overview />} />
               <Route path="/projects" element={<Project />} />
               <Route path="/server" element={<Server />} />
               <Route path="/alpr" element={<Alpr />} />
               <Route path="/speed" element={<Speed />} />
-              <Route path="/traffic-light-violation" element={<TrafficLightViolation />} />
+              <Route
+                path="/traffic-light-violation"
+                element={<TrafficLightViolation />}
+              />
               <Route path="/user-profile" element={<UserProfile />} />
               <Route path="/account" element={<Account />} />
               <Route path="/corporate" element={<Corporate />} />
               <Route path="/blog" element={<Blog />} />
               <Route path="/camera-list" element={<CameraList />} />
-              <Route path="/notification" element={<Notification />} /> */}
+              <Route path="/notification" element={<Notification />} />
             </Routes>
           </main>
         </div>
