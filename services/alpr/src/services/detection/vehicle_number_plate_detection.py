762b0d4d86ebb0f00cd965275b81a84dabd8b394
Ishwor Subedi <ishworhero21@gmail.com>
2024-02-05 14:40:46 +0545

    - feat: Initialize YOLO vehicle detection model - feat: Collect and preprocess training dataset for YOLO - feat: Implement vehicle detection service using YOLO - feat: Add multithreading support for image capture - feat: Integrate PaddleOCR for text recognition - feat: Train PaddleOCR with custom dataset - feat: Generate data for PaddleOCR training - feat: Implement threading concepts in PaddleOCR integration - refactor: Improve code structure and performance - docs: Update documentation with new features and usage instructions

---
+++ services/alpr/src/services/detection/vehicle_number_plate_detection.py
@@ -0,0 +1,143 @@
+from ultralytics.models.yolo import YOLO
+from services.alpr.src.entity.service_config import ServiceConfig
+import cv2
+
+
+class DetectionService:
+    def __init__(self, config: ServiceConfig):
+        self.model_instance = YOLO(config.model_path)
+
+    def detect_image(self, image_path, confidence_threshold: float, nms_threshold: float):
+        """
+        Detects vehicle number plate from an image
+        :param image_path:  image path to be passed
+        :param confidence_threshold:  confidence threshold
+        :param nms_threshold:  nms threshold
+        :return:  detected results
+        """
+
+        img = cv2.imread(image_path)
+        results = self.model_instance.predict(img, conf=confidence_threshold, iou=nms_threshold)
+
+        detections = []
+        for prediction in results:
+            bboxes = prediction.boxes.xyxy
+            try:
+                bbox = bboxes[0].int().tolist()
+                x1, y1, x2, y2 = bbox
+                cropped = img[y1:y2, x1:x2]
+                return prediction, cropped
+
+            except Exception as e:
+                print(e)
+
+    def detect_video(self, video_path: str, display: bool, confidence_threshold: float, nms_threshold: float):
+        """
+          Detects vehicle number plate from a video
+        :param video_path:  video path to be passed
+        :param display:  boolean value to display the video
+        :param confidence_threshold:  confidence threshold
+        :param nms_threshold: nms threshold
+        :return:  detected results
+        """
+        cap = cv2.VideoCapture(video_path)
+        detections = []
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Failed to read frame")
+                break
+            frame = cv2.resize(frame, (640, 480))  # Resize the frame
+            results = self.model_instance(frame, conf=confidence_threshold, iou=nms_threshold)
+            for prediction in results:
+                bboxes = prediction.boxes.xyxy
+                try:
+                    bbox = bboxes[0].int().tolist()
+                    x1, y1, x2, y2 = bbox
+                    cropped = frame[y1:y2, x1:x2]
+                    detections.append((prediction, cropped))
+                    if display:
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                        cv2.putText(frame, f'Detected', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+                except Exception as e:
+                    print(e)
+            if display:
+                cv2.imshow('frame', frame)
+                key = cv2.waitKey(1) & 0xFF
+                if key == ord('q'):
+                    cv2.destroyAllWindows()
+                    break
+        cap.release()
+        return detections
+
+    def detect_webcam(self, display: bool, confidence_threshold: float, nms_threshold: float):
+        """
+        Detects
+        vehicle
+        number
+        plate
+        from a webcam
+        :param
+        display: boolean
+        value
+        to
+        display
+        the
+        video
+        :param
+        confidence_threshold: confidence
+        threshold
+        :param
+        nms_threshold: nms
+        threshold
+        :return:  detected
+        results
+        """
+
+        cap = cv2.VideoCapture(0)
+        detections = []
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Failed to read frame")
+                break
+            frame = cv2.resize(frame, (640, 480))  # Resize the frame
+            results = self.model_instance(frame, conf=confidence_threshold, iou=nms_threshold)
+            for prediction in results:
+                bboxes = prediction.boxes.xyxy
+                try:
+                    bbox = bboxes[0].int().tolist()
+                    x1, y1, x2, y2 = bbox
+                    cropped = frame[y1:y2, x1:x2]
+                    detections.append((prediction, cropped))
+                    print(len(detections))
+                    if display:
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                        cv2.putText(frame, f'Detected', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+                except Exception as e:
+                    print(e)
+            if display:
+                cv2.imshow('frame', frame)
+                key = cv2.waitKey(1) & 0xFF
+                if key == ord('q'):
+                    cv2.destroyAllWindows()
+                    break
+        cap.release()
+        return detections
+
+
+if __name__ == '__main__':
+    config = ServiceConfig(model_path='services/alpr/resources/yolov8/nnpd.pt')
+    service = DetectionService(config)
+
+    # Detect webcam
+    service.detect_webcam(display=True, confidence_threshold=0.5, nms_threshold=0.4)
+
+    # Detect image
+    # service.detect_image('/home/ishwor/Pictures/Screenshots/Screenshot from 2024-02-03 20-41-34.png',
+    #                      confidence_threshold=0.5, nms_threshold=0.4)
+
+    # Detect video
+    # service.detect_video(
+    #     '/home/ishwor/Desktop/TreeLeaf/Vehicle Number Plate/Dataset/cam_back_2022_09_02/videos/ipcam__1.mp4',
+    #     display=True, confidence_threshold=0.5, nms_threshold=0.4)
